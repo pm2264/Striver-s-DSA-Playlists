5d979c11f4b9300bef3a3963a6efa27468385036
Pavitra Mohanarajan <114989541+pm2264@users.noreply.github.com>
2023-09-02 22:37:20 +0530

    Create Find Second Smallest and Second Largest Element in an array.py

---
+++ Find Second Smallest and Second Largest Element in an array.py
@@ -0,0 +1,16 @@
+def getElements(arr, n):
+    if n == 0 or n == 1:
+        print(-1, -1)  # edge case when only one element is present in array
+    arr.sort()
+    small = arr[1]
+    large = arr[n-2]
+    print("Second smallest is", small)
+    print("Second largest is", large)
+
+
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 4, 6, 7, 5]
+    n = len(arr)
+    getElements(arr, n)
